f5db9759d37de7e296654c91fe758ce60976860a
Dayoung <dayoungkim9105@gmail.com>
2022-12-29 03:45:39 +0900

    feat(server): routes 구성

---
+++ server/src/routes/messages.js
@@ -0,0 +1,93 @@
+import { v4 } from "uuid";
+import { readDB, writeDB } from "../dbController.js";
+
+const getMsgs = () => readDB("messages");
+const setMsgs = (data) => writeDB("messages", data);
+
+const messagesRoute = [
+  {
+    //전체메시지 가져오기
+    method: "get",
+    route: "/messages",
+    handler: (req, res) => {
+      const msgs = getMsgs();
+      res.send(msgs);
+    },
+  },
+
+  //아이디 하나에 대한 메시지 가져오기
+  {
+    method: "get",
+    route: "/messages/:id",
+    handler: ({ params: { id } }, res) => {
+      try {
+        const msgs = getMsgs();
+        const msg = msgs.find((m) => m.id === id);
+        if (!msg) throw Error("not found");
+        res.send(msg);
+      } catch (err) {
+        res.status(404).send({ error: err });
+      }
+    },
+  },
+  {
+    //메시지 만들기
+    method: "post",
+    route: "/messages",
+    handler: ({ body }, res) => {
+      const msgs = getMsgs();
+      const newMsg = {
+        id: v4(),
+        text: body.text,
+        userId: body.userId,
+        timestamp: Date.now(),
+      };
+      msgs.unshift(newMsg);
+      setMsgs(msgs);
+      res.send(newMsg);
+    },
+  },
+  {
+    //메시지 수정
+    method: "put",
+    route: "/messages/:id",
+    handler: ({ body, params: { id } }, res) => {
+      try {
+        const msgs = getMsgs();
+        const targetIndex = msgs.findIndex((msg) => msg.id === id);
+        if (targetIndex < 0) throw "메시지가 없습니다.";
+        if (msgs[targetIndex].userId !== body.userId)
+          throw "사용자가 다릅니다.";
+
+        const newMsg = { ...msgs[targetIndex], text: body.text };
+        msgs.splice(targetIndex, 1, newMsg);
+        setMsgs(msgs);
+        res.send(newMsg);
+      } catch (err) {
+        res.status(500).send({ error: err });
+      }
+    },
+  },
+  {
+    //메시지 삭제
+    method: "delete",
+    route: "/messages/:id",
+    handler: ({ body, params: { id } }, res) => {
+      try {
+        const msgs = getMsgs();
+        const targetIndex = msgs.findIndex((msg) => msg.id === id);
+        if (targetIndex < 0) throw "메시지가 없습니다.";
+        if (msgs[targetIndex].userId !== body.userId)
+          throw "사용자가 다릅니다.";
+
+        msgs.splice(targetIndex, 1);
+        setMsgs(msgs);
+        res.send(id);
+      } catch (err) {
+        res.status(500).send({ error: err });
+      }
+    },
+  },
+];
+
+export default messagesRoute;
