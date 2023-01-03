e420407c0b12ffd795440d71e74d1ab43360e1c4
Dayoung <dayoungkim9105@gmail.com>
2023-01-04 03:07:19 +0900

    chore(client): gql 적용

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
+import { ApolloServer } from "apollo-server-express";
 import messagesRoute from "./routes/messages.js";
 import usersRoute from "./routes/users.js";
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.use(
   cors({
@@ -14,10 +15,23 @@ app.use(
   })
 );
 
-const routes = [...messagesRoute, ...usersRoute];
-routes.forEach(({ method, route, handler }) => {
-  app[method](route, handler);
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    models: {
+      messages: "",
+      users: "",
+    },
+  },
 });
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+// const routes = [...messagesRoute, ...usersRoute];
+// routes.forEach(({ method, route, handler }) => {
+//   app[method](route, handler);
+// });
 
 app.listen(8000, () => {
   console.log("server listening 8000...");
